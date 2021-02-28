950ae0c1002d43140e0218409903e36ae61ba0b9
Igor Gilman <info@igor-gilman.de>
2021-02-28 11:47:01 +0100

    composer

--- examples/project.complex/_bootstrap/loader.php
+++ examples/project.complex/_bootstrap/loader.php
@@ -3,6 +3,6 @@
 use GI\Autoloader\Autoloader;
 
 
-require_once dirname(dirname(__DIR__)) . '/vendor/gi-framework/gi-framework/Autoloader/resources.php';
+require_once dirname(dirname(__DIR__)) . '/vendor/gi-framework/gi-framework/GI/Autoloader/resources.php';
 
 (new Autoloader())->addBaseNamespace('', dirname(__DIR__));
