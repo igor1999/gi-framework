42de76e0be3b4925fcf847e16ee6c8feb9b63f71
Igor Gilman <info@igor-gilman.de>
2021-03-05 05:10:16 +0100

    tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    include __DIR__ . '/vendor/autoload.php';
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    include dirname(__DIR__) . '/vendor/autoload.php';
 } else {
     throw new RuntimeException('vendor/autoload.php not found. Did you run `php composer.phar install`?');
 }
