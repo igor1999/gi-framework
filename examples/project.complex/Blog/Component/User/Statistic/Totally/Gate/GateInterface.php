fe05b1145de221862f992280af15e8578b6e54f0
Igor Gilman <info@igor-gilman.de>
2021-05-05 21:32:04 +0200

    user statistic table

---
+++ examples/project.complex/Blog/Component/User/Statistic/Totally/Gate/GateInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Blog\Component\User\Statistic\Totally\Gate;
+
+use Blog\Component\Base\Gate\UsecaseInterface as BaseInterface;
+
+interface GateInterface extends BaseInterface
+{
+
+}
